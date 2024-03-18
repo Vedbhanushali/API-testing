2a72df0fbd3a3d2fa2933d589e77358f8219d9a1
Vedbhanushali <19ce005@charusat.edu.in>
2024-03-18 15:18:20 +0530

    Stable v1

---
+++ tests/auth.setup.ts
@@ -0,0 +1,39 @@
+import { expect, test as setup } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const authFile = 'playwright/.auth/user.json';
+
+setup('Signing up the user', async ({ page, request }) => {
+    // sign up step
+    console.log(process.env.API_ENDPOINT)
+    const signupResponse = await request.post(`${process.env.API_ENDPOINT}/person/signup`, {
+        data: {
+            name: "Playwright",
+            email: process.env.email,
+            password: process.env.password,
+            age: 28,
+            type: "chef",
+            mobile: "234-234-2345",
+            address: "CA",
+            salary: 60000
+        }
+    });
+    console.log(signupResponse)
+    // expect(signupResponse.ok()).toBeTruthy();
+    //if already exist user do nothing
+    await page.context().storageState({ path: authFile })
+    //storage state no meanging (as not setting any cookie,session)
+
+    // // saving auth token in env variable by login
+    // const loginRes = await request.post(`${process.env.API_ENDPOINT}/person/login`, {
+    //     data: {
+    //         email: process.env.email,
+    //         password: process.env.password,
+    //     }
+    // });
+    // const responseBody = await loginRes.json();
+    // expect(loginRes.ok()).toBeTruthy();
+    // console.log("login token", responseBody.token)
+    // process.env.TOKEN = responseBody.token
+});
--- tests/person.spec.ts
+++ tests/person.spec.ts
@@ -1,18 +1,76 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
 
-//will give JWT token need to store
-test('login', async ({ request }) => {
+// Request context is reused by all tests in the file.
+let apiContext;
+test('Login', async ({ request, playwright }) => {
+  // login saving auth token in api context
+  const loginRes = await request.post(`${process.env.API_ENDPOINT}/person/login`, {
+    data: {
+      email: process.env.email,
+      password: process.env.password,
+    }
+  });
+  const responseBody = await loginRes.json();
+  expect(loginRes.ok()).toBeTruthy();
 });
 
 test.describe("Person API testing", () => {
   //all testcases require JWT token in requests
-  //WIP how to handle them
+  //JWT token in added in apiContext
+  test.beforeAll('Login and set apiContext', async ({ request, playwright }) => {
+    // login saving auth token in api context
+    const loginRes = await request.post(`${process.env.API_ENDPOINT}/person/login`, {
+      data: {
+        email: process.env.email,
+        password: process.env.password,
+      }
+    });
+    const responseBody = await loginRes.json();
+    expect(loginRes.ok()).toBeTruthy();
+    console.log("login token", responseBody.token)
+    apiContext = await playwright.request.newContext({
+      // All requests we send go to this API endpoint.
+      baseURL: `${process.env.API_ENDPOINT}`,
+      extraHTTPHeaders: {
+        'Authorization': `Bearer ${responseBody.token}`,
+      },
+    });
+  });
+
   test('profile', async ({ request }) => {
+    const profileRes = await apiContext.get('/person/profile')
+    console.log("Profile Response --> ", await profileRes.json())
+    expect(profileRes.ok()).toBeTruthy();
   });
+
   test('list all people', async ({ request }) => {
+    const profileRes = await apiContext.get('/person')
+    console.log("List of people --> ", await profileRes.json())
+    expect(profileRes.ok()).toBeTruthy();
   });
+
+  test('Filtered list all people', async ({ request }) => {
+    const type = 'chef' // type - ['chef','waiter','manager']
+    const profileRes = await apiContext.get(`/person/${type}`)
+    console.log("Filtered List of people --> ", await profileRes.json())
+    expect(profileRes.ok()).toBeTruthy();
+  });
+
   test('Edit personal details', async ({ request }) => {
+    const profileRes = await apiContext.put('/person', {
+      data: {
+        name: "Playwright modified"
+      }
+    })
+    console.log("Edited Profile Response --> ", await profileRes.json())
+    expect(profileRes.ok()).toBeTruthy();
   });
+
   test('Deleting personal profile', async ({ request }) => {
+    const profileRes = await apiContext.delete('/person')
+    console.log("Delete Profile Response --> ", await profileRes.json())
+    expect(profileRes.ok()).toBeTruthy();
   });
 });
